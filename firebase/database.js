482519ca29d3a495633fe9a42b31958ca0ea7202
edmund_opiyo <edmundopiyo@gmail.com>
2021-07-09 17:40:05 +0100

    crops

--- firebase/database.js
+++ firebase/database.js
@@ -1,14 +1,16 @@
 var firebase = require('firebase-admin');
 const serviceAccount = require('./modugreens-firebase-adminsdk-1b0v5-a9e1fa082e.json');
 var app = firebase.initializeApp({  
-    apiKey: "AIzaSyALR1DkAgjaoEmak1UVAvSxkfbG4zOSjxo",
-    authDomain: "modugreens.firebaseapp.com",
-    projectId: "modugreens",
-    storageBucket: "modugreens.appspot.com",
-    messagingSenderId: "830310619902",
-    appId: "1:830310619902:web:ea0cccf4f0067d100163af",
-    measurementId: "G-TZKY211NZR",
-    credential: admin.credential.cert(serviceAccount),
+   // apiKey: "AIzaSyALR1DkAgjaoEmak1UVAvSxkfbG4zOSjxo",
+    //authDomain: "modugreens.firebaseapp.com",
+    //projectId: "modugreens",
+   // storageBucket: "modugreens.appspot.com",
+    //messagingSenderId: "830310619902",
+    //appId: "1:830310619902:web:ea0cccf4f0067d100163af",
+    //measurementId: "G-TZKY211NZR",
+    credential: firebase.credential.cert(serviceAccount),
+    //databaseURL: "https://modugreens-default-rtdb.firebaseio.com",
+    //storageBucket: process.env.STORAGE_BUCKET
 
 });
 module.exports = firebase;
