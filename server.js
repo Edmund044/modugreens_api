5f3dd481c4df4a0e2ca17ea2e08a17a05d233186
edmund_opiyo <edmundopiyo@gmail.com>
2021-08-10 07:24:34 +0100

    routes for customer

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ const http = require('http');
 const app = require('./api/app');
 //const express = require('express');
 //const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 4000;
 const server = http.createServer(app);
 server.listen(port,()=>{
-    console.log("Server running at port 3000");
+    console.log("Server running at port 4000");
 });
