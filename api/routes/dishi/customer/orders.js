8acdd56b47117bccb8ec37469a831a1a36f27841
edmund044 <edmundopiyo@gmail.com>
2021-09-04 19:46:34 +0100

    all 3

---
+++ api/routes/dishi/customer/orders.js
@@ -0,0 +1,107 @@
+const express = require('express');
+const app = express();
+
+require('dotenv').config();
+const admin = require('../../../../firebase/database');
+const db = admin.firestore();
+
+
+//get about 
+app.get("/orders",async (req,res,next)=>{
+  const snapshot = await db.collection("orders")
+                  .get()
+                  .then( (snapshot) => {
+                    const data = snapshot.docs.map((doc) => ({ id:doc.id,...doc.data() }));
+                    res.status(200).json(data); 
+                    console.log(data);
+                  }
+                   
+                  )
+                  .catch( 
+                    error => {
+                    res.status(500).json({error:error})                   
+                  });
+});
+//get about 
+app.get("/orders/:id",async (req,res,next)=>{
+  const id = req.params.id;
+  console.log(id);
+  const snapshot = await db.collection("orders")
+                  .where(admin.firestore.FieldPath.documentId(), "==", id)
+                  .get()
+                  .then(
+                    (snapshot) => {
+                    const data = snapshot.docs.map((doc) => ({ id:doc.id,...doc.data() }));
+                    res.status(200).json(data); 
+                    console.log(data); }                          
+                  )
+                  .catch( 
+                    error => {
+                    res.status(500).json({error:error})                   
+                  });
+});
+//post about
+app.post("/orders",async (req,res,next) =>{
+  const data = req.body;
+    let snapshot= await db.collection("orders")
+        .add(data)
+        .then(
+           (snapshot) => {
+            res.status(200).json({message:"Done"});
+           } 
+         
+        )
+        .catch(
+            error => {
+                res.status(500).json({error:error})                   
+              }
+        );
+
+});
+//update about
+
+app.put("/orders",async (req,res,next)=>{
+    const id = req.body.id;
+    delete req.body.id;
+    const data = req.body;
+    let snapshot= await db.collection("orders")
+        .doc(id)
+        .update({
+        data
+                })
+        .then(
+           (snapshot) => {
+             res.status(200).json({message:"Done"});
+               } 
+            )
+        .catch(
+            error => {
+            res.status(500).json({error:error})                   
+               }
+              );        
+ 
+});
+
+//delete about
+app.delete("/orders",async (req,res,next) =>{
+  const id = req.body.id;
+  delete req.body.id;
+  const data = req.body;
+  let snapshot= await db.collection("orders")
+      .doc(id)
+      .delete({
+      data
+              })
+      .then(
+         (snapshot) => {
+           res.status(200).json({message:"Done"});
+             } 
+          )
+      .catch(
+          error => {
+          res.status(500).json({error:error})                   
+             }
+            );  
+});
+
+module.exports = app;
---
+++ api/routes/matatu/matatu.js
@@ -0,0 +1,107 @@
+const express = require('express');
+const app = express();
+
+require('dotenv').config();
+const admin = require('../../../../firebase/database');
+const db = admin.firestore();
+
+
+//get about 
+app.get("/crops",async (req,res,next)=>{
+  const snapshot = await db.collection("crops")
+                  .get()
+                  .then( (snapshot) => {
+                    const data = snapshot.docs.map((doc) => ({ id:doc.id,...doc.data() }));
+                    res.status(200).json(data); 
+                    console.log(data);
+                  }
+                   
+                  )
+                  .catch( 
+                    error => {
+                    res.status(500).json({error:error})                   
+                  });
+});
+//get about 
+app.get("/crops/:id",async (req,res,next)=>{
+  const id = req.params.id;
+  console.log(id);
+  const snapshot = await db.collection("crops")
+                  .where(admin.firestore.FieldPath.documentId(), "==", id)
+                  .get()
+                  .then(
+                    (snapshot) => {
+                    const data = snapshot.docs.map((doc) => ({ id:doc.id,...doc.data() }));
+                    res.status(200).json(data); 
+                    console.log(data); }                          
+                  )
+                  .catch( 
+                    error => {
+                    res.status(500).json({error:error})                   
+                  });
+});
+//post about
+app.post("/crops",async (req,res,next) =>{
+  const data = req.body;
+    let snapshot= await db.collection("crops")
+        .add(data)
+        .then(
+           (snapshot) => {
+            res.status(200).json({message:"Done"});
+           } 
+         
+        )
+        .catch(
+            error => {
+                res.status(500).json({error:error})                   
+              }
+        );
+
+});
+//update about
+
+app.put("/crops",async (req,res,next)=>{
+    const id = req.body.id;
+    delete req.body.id;
+    const data = req.body;
+    let snapshot= await db.collection("crops")
+        .doc(id)
+        .update({
+        data
+                })
+        .then(
+           (snapshot) => {
+             res.status(200).json({message:"Done"});
+               } 
+            )
+        .catch(
+            error => {
+            res.status(500).json({error:error})                   
+               }
+              );        
+ 
+});
+
+//delete about
+app.delete("/crops",async (req,res,next) =>{
+  const id = req.body.id;
+  delete req.body.id;
+  const data = req.body;
+  let snapshot= await db.collection("crops")
+      .doc(id)
+      .delete({
+      data
+              })
+      .then(
+         (snapshot) => {
+           res.status(200).json({message:"Done"});
+             } 
+          )
+      .catch(
+          error => {
+          res.status(500).json({error:error})                   
+             }
+            );  
+});
+
+module.exports = app;
