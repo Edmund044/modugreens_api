99e3cb77129aacf47995d638983241fbfdcc3df8
edmund044 <edmundopiyo@gmail.com>
2021-11-24 18:14:30 +0000

    instruction

--- api/routes/waterLevel/status/status.js
+++ api/routes/waterLevel/status/status.js
@@ -27,7 +27,7 @@ app.get("/status/:id",async (req,res,next)=>{
   const id = req.params.id;
   console.log(id);
   const snapshot = await db.collection("status")
-                  .where(admin.firestore.FieldPath.documentId(), "==", id)
+                  .where("unique_number", "==", id)
                   .get()
                   .then(
                     (snapshot) => {
